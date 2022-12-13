a472a1d32f99967bdd1540a59fea4b5f81b800cd
Uğur Kağan Çakır <ugurkagan@sabanciuniv.edu>
2022-12-13 17:51:47 +0300

    minor changes

--- opengl/maincpp.cpp
+++ opengl/maincpp.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
-#include<stb/stb_image.h>
+#include"stb_image.h"
 #include<glm/glm.hpp>
 #include<glm/gtc/matrix_transform.hpp>
 #include<glm/gtc/type_ptr.hpp>
